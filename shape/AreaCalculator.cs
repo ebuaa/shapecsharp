fe9a00ded58ac175ff1fb38bb8e834db6d027692
ebuaa <axel.aube@viacesi.fr>
2024-03-27 09:36:00 +0100

    files created

---
+++ shape/AreaCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shape
+{
+    public class AreaCalculator { 
+        public double TotalArea(Rectangle[] arrRectangles)
+        {
+            double area;
+            foreach (var objRectangle in arrRectangles)
+            {
+                area += objRectangle.Height * objRectangle.Width;
+            }
+            return area;
+        }
+    }
+}
